80229345d4145bc5ba85cf46d39adcc4c4b6e2d8
amleshwar-p <vinesnimish@gmail.com>
2024-12-09 18:55:38 +0530

    Added env in firebase

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/
 
 // Your Firebase config (replace these with your own Firebase credentials)
 const firebaseConfig = {
-    apiKey: "AIzaSyB-39CRp3xoZC5HuK5Urh2wSY_pNjuqDjI",
-    authDomain: "wordracer-e3cbd.firebaseapp.com",
-    projectId: "wordracer-e3cbd",
-    storageBucket: "wordracer-e3cbd.firebasestorage.app",
-    messagingSenderId: "167576538467",
-    appId: "1:167576538467:web:69e1eda5d24ef5f9215756",
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
